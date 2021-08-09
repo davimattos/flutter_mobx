bf87e86790fb354f1fdd5613b6a3e77cc90ca1b8
mattosdavi <davi.mattos@thoughtworks.com>
2021-08-09 10:17:51 -0300

    criando e implementando layout do cronometro

--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro_mobx/components/cronometro.dart';
 import 'package:flutter_pomodoro_mobx/components/entrada_tempo.dart';
 
 class Pomodoro extends StatelessWidget {
@@ -8,12 +9,18 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            EntradaTempo(titulo: 'Trabalho', valor: 25),
-            EntradaTempo(titulo: 'Descanso', valor: 5)
-          ])
+          Expanded(child: Cronometro()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  EntradaTempo(titulo: 'Trabalho', valor: 25),
+                  EntradaTempo(titulo: 'Descanso', valor: 5)
+                ]),
+          )
         ],
       ),
     );
